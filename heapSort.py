ba7452a440ebb9ef3c885f2aea200bf2dcb9bfa3
wenbinli <wenbinli@mpi-inf.mpg.de>
2017-08-09 21:51:20 +0200

    improved version for maxHeapify

--- heapSort.py
+++ heapSort.py
@@ -1,33 +1,54 @@
 # heap sort experiment
+# Version 1
+# def maxHeapify(data,idx):
+#     if idx < len(data):
+#         root = data[idx]
+#     else:
+#         return data
+
+#     # only compare if there are children nodes for the current index
+#     if 2 * idx + 1 < len(data):
+#         l_child = data[2*idx + 1]
+#     else:
+#         l_child = -10 # surrogate for -inf
+
+#     if 2 * idx + 2 < len(data):    
+#         r_child = data[2*idx + 2]
+#     else:
+#         r_child = -10
+
+#     # root exchanges with bigger child
+#     if l_child > root and l_child > r_child:
+#         data[idx] = l_child
+#         data[2 * idx + 1] = root
+#     if r_child > root and r_child > l_child:
+#         data[idx] = r_child
+#         data[2 * idx + 2] = root
+
+#     # check if max-heap properties maintains at sub-tree
+#     maxHeapify(data,2*idx + 1)
+#     maxHeapify(data,2*idx + 2)
+
+#     return data
+
+# Version 2
 def maxHeapify(data,idx):
-    if idx < len(data):
-        root = data[idx]
-    else:
-        return data
+    largest = idx
+    l = 2 * idx + 1
+    r = 2 * idx + 2
+    n = len(data)
 
-    # only compare if there are children nodes for the current index
-    if 2 * idx + 1 < len(data):
-        l_child = data[2*idx + 1]
-    else:
-        l_child = -10 # surrogate for -inf
+    if l < n and data[largest] < data[l]:
+        largest = l
 
-    if 2 * idx + 2 < len(data):    
-        r_child = data[2*idx + 2]
-    else:
-        r_child = -10
+    if r < n and data[largest] < data[r]:
+        largest = r
 
-    # root exchanges with bigger child
-    if l_child > root and l_child > r_child:
-        data[idx] = l_child
-        data[2 * idx + 1] = root
-    if r_child > root and r_child > l_child:
-        data[idx] = r_child
-        data[2 * idx + 2] = root
+    if largest != idx:
+        data[idx], data[largest] = data[largest], data[idx]
 
-    # check if max-heap properties maintains at sub-tree
-    maxHeapify(data,2*idx + 1)
-    maxHeapify(data,2*idx + 2)
-
+        maxHeapify(data, largest)
+        
     return data
 
 data = [4,10,3,5,1]
