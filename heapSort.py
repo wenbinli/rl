a1205b6963eee52afeb744a00b85742906450542
wenbinli <wenbinli@mpi-inf.mpg.de>
2017-08-09 21:30:20 +0200

    trial version for maxHeapify

--- heapSort.py
+++ heapSort.py
@@ -1,7 +1,35 @@
 # heap sort experiment
-def heapify(data):
+def maxHeapify(data,idx):
+    if idx < len(data):
+        root = data[idx]
+    else:
+        return data
 
+    # only compare if there are children nodes for the current index
+    if 2 * idx + 1 < len(data):
+        l_child = data[2*idx + 1]
+    else:
+        l_child = -10 # surrogate for -inf
+
+    if 2 * idx + 2 < len(data):    
+        r_child = data[2*idx + 2]
+    else:
+        r_child = -10
+
+    # root exchanges with bigger child
+    if l_child > root and l_child > r_child:
+        data[idx] = l_child
+        data[2 * idx + 1] = root
+    if r_child > root and r_child > l_child:
+        data[idx] = r_child
+        data[2 * idx + 2] = root
+
+    # check if max-heap properties maintains at sub-tree
+    maxHeapify(data,2*idx + 1)
+    maxHeapify(data,2*idx + 2)
+
+    return data
 
 data = [4,10,3,5,1]
-data_sort = heapify(data)
+data_sort = maxHeapify(data,0)
 print data_sort
